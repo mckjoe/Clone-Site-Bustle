8f35e90018cb4b89498282364522a2a13a5b50fc
joe mckinney <mckinney.a.joe@gmail.com>
2018-05-11 10:27:04 -0700

    update project so that edit form shows up in admin view, and will change location of the click function to being in a div that wraps the image and title and author of my article so users can use update form without being routed to the viewArticle page.

--- src/app/edit-article/edit-article.component.ts
+++ src/app/edit-article/edit-article.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Article } from '../article.model';
 
 @Component({
   selector: 'app-edit-article',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-article.component.css']
 })
 export class EditArticleComponent implements OnInit {
+  @Input() selectedArticle;
 
   constructor() { }
 
--- src/app/list-page/list-page.component.ts
+++ src/app/list-page/list-page.component.ts
@@ -12,7 +12,7 @@ import { FirebaseListObservable } from 'angularfire2/database';
 export class ListPageComponent implements OnInit {
   articles: FirebaseListObservable<any[]>;
   currentRoute: string = this.router.url;
-  
+
   constructor(private router: Router, private articleService: ArticleService) { }
 
   ngOnInit() {
