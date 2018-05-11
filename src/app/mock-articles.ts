25ef7c3ebdeb9eba5ac2a0fddb61cf3d75449936
joe mckinney <mckinney.a.joe@gmail.com>
2018-05-11 08:30:04 -0700

    brought back moch articles and commented them back out

--- src/app/mock-articles.ts
+++ src/app/mock-articles.ts
@@ -1,5 +1,5 @@
 // import { Article } from './article.model';
-
+//
 // export const ARTICLES:  Article[] = [
 //   new Article("Example Title", "Welcome to bustleClone the website!", "Joseph McKinney", "This webpage is a webpage that allows users to view articles.  This is a pretend article that you are reading right now it has no relevence.", "https://www.askideas.com/media/12/I-Just-Like-To-Smile-Funny-Random-Funny-Random.jpg", 1),
 //   new Article("Example 2", "this is another example", "Random Author", "This is another pretend article, hopefully it has been for of education","https://www.askideas.com/media/47/Eating-Watermelon-Like-A-Boss-Funny-Cool-Meme-Image.jpg", 2),
