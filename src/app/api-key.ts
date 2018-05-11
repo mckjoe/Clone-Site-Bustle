c612e728d41310a0c5cbf20da6829a9daed22d58
joe mckinney <mckinney.a.joe@gmail.com>
2018-05-11 13:24:11 -0700

    deploy website to firebase.

---
+++ src/app/api-key.ts
@@ -0,0 +1,8 @@
+export let masterFirebaseConfig = {
+  apiKey: "AIzaSyCfozmPc-m7HUGZt1l9IDzsWyajdqEW-6I",
+  authDomain: "bustle-clone-1a696.firebaseapp.com",
+  databaseURL: "https://bustle-clone-1a696.firebaseio.com",
+  projectId: "bustle-clone-1a696",
+  storageBucket: "bustle-clone-1a696.appspot.com",
+  messagingSenderId: "240935625291"
+};
